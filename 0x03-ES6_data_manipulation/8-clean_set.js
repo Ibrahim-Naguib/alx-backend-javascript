ad7e60933e2c0fa73638b31ab65cdc8ace8e46cd
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-07-01 19:38:06 +0000

    new commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
   return [...set].filter((item) => item.startsWith(startString)).map((item) => item.slice(startString.length)).join('-');
 }
